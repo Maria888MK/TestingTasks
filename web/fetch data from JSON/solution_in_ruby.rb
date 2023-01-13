c716122f7ed506c6937754ca0de7e1e1ff05ca02
Maria Karpei <87585995+Maria888MK@users.noreply.github.com>
2023-01-13 15:47:11 +0100

    Update solution_in_ruby.rb

--- web/fetch data from JSON/solution_in_ruby.rb
+++ web/fetch data from JSON/solution_in_ruby.rb
@@ -1,34 +1,40 @@
+# frozen_string_literal: true
+
+# import required modules
 require 'net/http'
 require 'uri'
 require 'json'
 require 'open-uri'
 
-while true
-  # Create the HTTP objects
+loop do
+  # source URL to fetch the data
   uri = URI('https://www.reddit.com/r/subreddit.json')
+
+  # put your webhook url here
   webhook_url = 'https://webhook.site/387a6309-3688-4457-b4f1-193ea702bc24'
+
   json = Net::HTTP.get(uri)
-
-
-  # puts elements
   data_hash = JSON.parse(json)
-  # elements = data_hash['data']['dist']
+
+  # create hash object
   extracted_data = {}
 
-
+  # fill in the hash object using the extracted json data( title, url)
   data_hash['data']['children'].each do |child|
     title = child['data']['title']
     url_from_json = child['data']['url']
     extracted_data[title] = url_from_json
   end
+
+  # REVIEW: the hash object in the console
   extracted_data_json_format = JSON.pretty_generate(extracted_data)
   puts extracted_data_json_format
-  # Post the data to endpoint server
+
+  # push data to webhook.site
   webhook_uri = URI(webhook_url)
   res = Net::HTTP.post_form(webhook_uri, extracted_data)
-  puts res.body  if res.is_a?(Net::HTTPSuccess)
-  # encoded_params = URI.encode_www_form(extracted_data)
-  # response = Faraday.post(webhook_url, encoded_params)
-  # p response.body if response.status == 201
+  puts res.body if res.is_a?(Net::HTTPSuccess)
+  
+  # repeat the entire script each minute
   sleep 60
 end
